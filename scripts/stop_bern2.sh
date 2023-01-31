34ab8684896b8f72e0f4432dc896dfdc1908c39a
Alexander Belikov <abelikov@gmail.com>
2023-01-31 15:53:29 +0100

    no need to stop tmvar2 (handled by systemd)

--- scripts/stop_bern2.sh
+++ scripts/stop_bern2.sh
@@ -16,13 +16,13 @@ else
   echo "No GNormPlusServer.main.jar found to stop."
 fi
 
-pid=`ps auxww | grep tmVar2Server.main.jar | grep -v grep | awk '{print $2}' | sort -r`
-if [ "$pid" != "" ]; then
-  kill -9 "$pid"
-  echo "Stopped tmVar2Server.main.jar"
-else
-  echo "No tmVar2Server.main.jar found to stop."
-fi
+#pid=`ps auxww | grep tmVar2Server.main.jar | grep -v grep | awk '{print $2}' | sort -r`
+#if [ "$pid" != "" ]; then
+#  kill -9 "$pid"
+#  echo "Stopped tmVar2Server.main.jar"
+#else
+#  echo "No tmVar2Server.main.jar found to stop."
+#fi
 
 pid=`ps auxww | grep disease_normalizer_21.jar | grep -v grep | awk '{print $2}' | sort -r`
 if [ "$pid" != "" ]; then
