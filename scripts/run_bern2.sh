523b72ba98028187e4d6fdfb17a3ca13e5ae5259
Alexander Belikov <abelikov@gmail.com>
2023-02-02 00:40:19 +0100

    switched to systemd

--- scripts/run_bern2.sh
+++ scripts/run_bern2.sh
@@ -14,12 +14,12 @@ mkdir logs
 #    --mtner_home multi_ner \
 #    --mtner_port 18894 >> logs/nohup_multi_ner.out 2>&1 &
 
-cd resources
+#cd resources
 
 # run gnormplus
-cd GNormPlusJava
-nohup java -Xmx16G -Xms16G -jar GNormPlusServer.main.jar 18895 >> ../../logs/nohup_gnormplus.out 2>&1 &
-cd ..
+#cd GNormPlusJava
+#nohup java -Xmx16G -Xms16G -jar GNormPlusServer.main.jar 18895 >> ../../logs/nohup_gnormplus.out 2>&1 &
+#cd ..
 
 # run tmVar
 # cd tmVarJava
@@ -31,35 +31,34 @@ cd ..
 ####################################
 
 # Disease (working dir: normalization/)
-cd normalization
-nohup java -Xmx16G -jar normalizers/disease/disease_normalizer_21.jar \
-    "inputs/disease" \
-    "outputs/disease" \
-    "dictionary/dict_Disease_20210630.txt" \
-    "normalizers/disease/resources" \
-    9 \
-    18892 \
-    >> ../../logs/nohup_disease_normalize.out 2>&1 &
+#cd normalization
+#nohup java -Xmx16G -jar normalizers/disease/disease_normalizer_21.jar \
+#    "inputs/disease" \
+#    "outputs/disease" \
+#    "dictionary/dict_Disease_20210630.txt" \
+#    "normalizers/disease/resources" \
+#    9 \
+#    18892 \
+#    >> ../../logs/nohup_disease_normalize.out 2>&1 &
 
 # Gene (working dir: normalization/normalizers/gene/, port:18888)
-cd normalizers/gene
-nohup java -Xmx20G -jar gnormplus-normalization_21.jar \
-    18888 \
-    >> ../../../../logs/nohup_gene_normalize.out 2>&1 &
-cd ../../../..
+#cd normalizers/gene
+#nohup java -Xmx20G -jar gnormplus-normalization_21.jar \
+#    18888 \
+#    >> ../../../../logs/nohup_gene_normalize.out 2>&1 &
+#cd ../../../..
 
 ####################################
 #####       Run BERN2          #####
 ####################################
-poetry run python -u server.py \
-    --mtner_home ./multi_ner \
-    --mtner_port 18894 \
-    --gnormplus_home ./resources/GNormPlusJava \
-    --gnormplus_port 18895 \
-    --tmvar2_home ./resources/tmVarJava \
-    --tmvar2_port 18896 \
-    --gene_norm_port 18888 \
-    --disease_norm_port 18892 \
-    --use_neural_normalizer \
-    --port 8888 
-
+#poetry run python -u server.py \
+#    --mtner_home ./multi_ner \
+#    --mtner_port 18894 \
+#    --gnormplus_home ./resources/GNormPlusJava \
+#    --gnormplus_port 18895 \
+#    --tmvar2_home ./resources/tmVarJava \
+#    --tmvar2_port 18896 \
+#    --gene_norm_port 18888 \
+#    --disease_norm_port 18892 \
+#    --use_neural_normalizer \
+#    --port 8888
